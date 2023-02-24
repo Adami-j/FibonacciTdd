53ae68451b46b46d0741e9d71182439c4bc952fe
mkltr <mkltr309@gmail.com>
2023-02-24 22:04:43 +0100

    Création de la classe fiboIterator et de la classe de test associée

---
+++ src/FiboIterrator.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+class FiboIterator implements Iterator<Integer> {
+
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return null;
+    }
+}
