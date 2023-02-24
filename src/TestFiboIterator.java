b882065f841bb25462f565ecdacb963698b6fe7b
mkltr <mkltr309@gmail.com>
2023-02-24 22:06:07 +0100

    Ajout testFibo0 bloquage du test

--- src/TestFiboIterator.java
+++ src/TestFiboIterator.java
@@ -1,8 +1,15 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class TestFiboIterator {
 
+
+    @Test
+    public void testFibo0(){
+        FiboIterator fibo = new FiboIterator(0);
+        assertEquals(0, fibo.next().intValue());
+    }
 
 }
