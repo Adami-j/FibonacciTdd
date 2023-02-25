b0be04b62cc0713d3a5bad65cbe91f0ba66bface
mkltr <mkltr309@gmail.com>
2023-02-25 17:41:37 +0100

    Remise à 0 des classe et on recommence le TDD

--- src/FiboIterrator.java
+++ src/FiboIterrator.java
@@ -2,30 +2,14 @@ import java.util.Iterator;
 
 class FiboIterator implements Iterator<Integer> {
 
-    int depart;
-    int actuel;
-    int arrivee;
-    public FiboIterator(int arrivee) {
-        this.arrivee = arrivee;
-        this.depart = 0;
-        actuel = 0;
-    }
 
     @Override
     public boolean hasNext() {
-
-        return depart < arrivee;
+        return false;
     }
 
     @Override
     public Integer next() {
-        int resultat=0;
-        if(arrivee==0){
-            resultat=0;
-        }else if(arrivee==1){
-            resultat=1;
-        }
-        depart++;
-        return resultat;
+        return null;
     }
 }
---
+++ src/Fibonacci.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+
+public class Fibonacci implements Iterable<Integer> {
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
