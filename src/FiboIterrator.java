7725428ffe75efe71237a111d9106ac12d20c290
mkltr <mkltr309@gmail.com>
2023-02-25 19:00:21 +0100

    modification du next()

--- src/FiboIterrator.java
+++ src/FiboIterrator.java
@@ -2,8 +2,11 @@ import java.util.Iterator;
 
 class FiboIterator implements Iterator<Integer> {
 
+
     private int range;
     private int current = 0;
+    //parametre init à 1 car aucun terme de la suite ne peut être inférieur à 1
+    private int previous = 1;
 
     public FiboIterator(int range) {
         this.range = range;
@@ -16,6 +19,17 @@ class FiboIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-        return null;
+        int result;
+
+        if(this.current==0){
+           result = 1;
+        }else {
+            result = current+ previous;
+            previous = current;
+            System.out.println(result);
+
+        }
+        this.current++;
+        return result;
     }
 }
