d4cb310e902330ef152a40a92e6a500e9f1d05fe
mkltr <mkltr309@gmail.com>
2023-02-25 17:43:32 +0100

    ajout de la méthode de test testFiboIteratorHasNext qui teste si fibo(0) à un suivant

--- src/TestFiboIterator.java
+++ src/TestFiboIterator.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TestFiboIterator {
+
+    @Test
+    public void testFiboIteratorHasNext() {
+        FiboIterator fiboIterator = new FiboIterator(0);
+        assertFalse(fiboIterator.hasNext());
+    }
 }
