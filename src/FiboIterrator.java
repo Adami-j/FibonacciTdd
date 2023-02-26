3788ea80568c0d000591ae1a804d04591e4aa4dd
mkltr <mkltr309@gmail.com>
2023-02-26 10:14:45 +0100

    modification des noms variables de classe  + refactoring et amélioration de la méthode next

--- src/FiboIterrator.java
+++ src/FiboIterrator.java
@@ -3,33 +3,42 @@ import java.util.Iterator;
 class FiboIterator implements Iterator<Integer> {
 
 
-    private int range;
-    private int current = 0;
+    private int indexEntree;
+    private int indexCourrant = 0;
+    private int termeCourrant = 1;
+    private int termeSuivant = 1;
+
+
     //parametre init à 1 car aucun terme de la suite ne peut être inférieur à 1
     private int previous = 1;
 
-    public FiboIterator(int range) {
-        this.range = range;
+    public FiboIterator(int indexEntree) {
+        this.indexEntree = indexEntree;
     }
 
     @Override
     public boolean hasNext() {
-        return current < range;
+        return indexCourrant < indexEntree;
     }
 
     @Override
     public Integer next() {
-        int result;
-
-        if(this.current==0){
-           result = 1;
+        indexCourrant++;
+        int resultat = 0;
+        if(indexCourrant== 0){
+            return 1;
+        }else if(indexCourrant== 1){
+            termeCourrant = 1;
+            resultat = termeCourrant;
+            return 1;
+        }else if(indexCourrant== 2){
+            resultat = termeCourrant;
+            return 1;
         }else {
-            result = current+ previous;
-            previous = current;
-            System.out.println(result);
-
+            resultat = termeCourrant + termeSuivant;
+            termeSuivant = resultat;
+           // System.out.println("indexCourrant: " + indexCourrant+ " indexEntree: " + indexEntree + " termeCourrant: " + termeCourrant + " termeSuivant: " + termeSuivant + " resultat: " + resultat);
         }
-        this.current++;
-        return result;
+        return resultat;
     }
 }
