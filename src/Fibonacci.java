828374b82e30fc79e3a8135b24573c4938180b0c
mkltr <mkltr309@gmail.com>
2023-02-26 11:09:51 +0100

    ajout du contructeur de la classe et de l'itérator de FiboIterator ajout de la classe psvm pour tester l'iterator pour les 46 premières valeurs de fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,9 +2,22 @@ import java.util.Iterator;
 
 public class Fibonacci implements Iterable<Integer> {
 
+    private int indexEntree;
+
+    public Fibonacci(int indexEntree) {
+        this.indexEntree = indexEntree;
+    }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new FiboIterator(indexEntree);
+    }
+
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci(46);
+        for (Integer integer : fibonacci) {
+            System.out.println(integer);
+        }
     }
 }
